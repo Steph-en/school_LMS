79554247831cd5a3e2d6ab497cecbba3a959b267
Steph-en <appahstephen9@gmail.com>
2022-11-29 10:32:44 +0000

    CSS fix

--- asset/js/login.js
+++ asset/js/login.js
@@ -23,22 +23,39 @@ function getInfo()
     var username = document.getElementById("floatingInput").value
     var password = document.getElementById("floatingPassword").value
     var button = document.querySelector(".btn")
-
     // console.log("UserID: " + username + " and Password: " + password)
-    
     var i;
 
     for (i = 0; i < objPeople.length; i++) {
         if (username == objPeople[i].username && password == objPeople[i].password) {
-            
             alert(username + " is valid!!!")
-            
-            this.innerHTML = "<div class='loader'></div>"
-            setTimeout(() => {
-                this.innerHTML = "<button class='w-100 btn btn-lg btn-primary' type='button' onclick='getInfo()'><a href='/home.html'>Sign in</a></button>"
-            }, 1000)
+            return
         } 
     }
-    // alert("Invalid Userneme or Password")
-    return
+    console.log("Incorrect username or password")
 };
+
+// Firebase
+// Import the functions you need from the SDKs you need
+// import { initializeApp } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-app.js";
+// import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-analytics.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//     apiKey: "AIzaSyDkMNH30esem4etAjEnbBbOJp4S3-V5DXs",
+//     authDomain: "school-system-baa4d.firebaseapp.com",
+//     projectId: "school-system-baa4d",
+//     storageBucket: "school-system-baa4d.appspot.com",
+//     messagingSenderId: "1043186853877",
+//     appId: "1:1043186853877:web:bc1aa342e36e8b7d5c62bc",
+//     measurementId: "G-0R5P8SQW54"
+// };
+
+// Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+
+// const auth = firebase.auth();
